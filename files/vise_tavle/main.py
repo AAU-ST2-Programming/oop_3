b95fa9b8655a080bc913bd3a2ad7629689cce4c5
Martin Work PC <15941171+nurrix@users.noreply.github.com>
2026-02-24 14:57:20 +0100

    Final changes after lecture

--- files/vise_tavle/main.py
+++ files/vise_tavle/main.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import matplotlib
+matplotlib.use("QtAgg")
+import matplotlib.pyplot as plt
+
+filename = "C:/Users/Martin/Documents/ST2_AP_all_lectures/oop_3/files/data1.csv"
+data = []
+with open(filename) as f:
+    header = f.readline()
+    for line in f:
+        data.append(float(line.strip()))
+
+plt.plot(data)
+plt.show()
+
